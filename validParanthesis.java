defb55e0702956f6e907c3b887f95ffd7ceee8ce
Kalyan Goud_Nagaram <77835963+kalyan1828@users.noreply.github.com>
2024-08-29 17:05:50 +0530

    Create validParanthesis.java

---
+++ validParanthesis.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+class validParanthesis {
+    
+    public boolean isValid(String s){
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='{'){
+                stack.push('}');
+            }
+            else if(ch=='['){
+                stack.push(']');
+            }
+            else if(ch=='('){
+                stack.push(')');
+            }
+            else if (stack.isEmpty() || stack.pop() != ch) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        System.out.println("Try programiz.pro");
+        validParanthesis v=new validParanthesis();
+        
+        System.out.println(v.isValid("[{}]{]}"));
+        
+    }
+}
